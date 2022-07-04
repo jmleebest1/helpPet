4d7421bbe4a00eaaff53451e84d8dbf101ef9a7c
jmleebest1 <skminy27@nate.com>
2022-07-04 23:21:44 +0900

    프로젝트 환경설정

---
+++ src/main/java/com/example/helppet/HelppetApplication.java
@@ -0,0 +1,13 @@
+package com.example.helppet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelppetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelppetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helppet/HelppetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helppet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelppetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
